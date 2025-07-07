1d86d0ce6a1553c33c8e7ef58e23f99c946c4f16
Elwalid <elwalid.bousmaha@outlook.com>
2025-07-07 11:46:06 +0100

    Use .env for secret settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -22,12 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i7qpx9fwc2p(_)*ypjn!yzr9)e**x)@g!3!!qv$j^=nhz)l600'
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
@@ -125,8 +124,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'cricarsonline@gmail.com'
-EMAIL_HOST_PASSWORD = 'qgbp cevh wapv botg'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Media files (Images uploaded through ImageField)
