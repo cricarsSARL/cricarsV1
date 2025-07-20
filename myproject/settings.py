bf007c6f41065bcbfd5ef89e1d4f254b0366e4cb
Elwalid <elwalid.bousmaha@outlook.com>
2025-07-20 18:00:53 +0100

    Update settings and requirements for PostgreSQL integration

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,6 +81,8 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
